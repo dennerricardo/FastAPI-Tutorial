27e140c0f4ab280d3951bcc1b844deb39d96ab29
dennerricardo <denner.ricardo@gmail.com>
2026-07-01 09:26:42 -0300

    fast api tutorial: get(/feed)

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@ from app.schemas import  PostCreate, PostResponse
 from app.db import Post, create_db_and_tables, get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
+from sqlalchemy import select
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -16,7 +17,7 @@ app = FastAPI(lifespan=lifespan)
 async def upload_file(
         file: UploadFile = File(...),
         caption: str = Form(""),
-        session: AsyncSession = Depends(get_async_session())
+        session: AsyncSession = Depends(get_async_session)
 ):
     post = Post(
         caption=caption,
@@ -27,7 +28,26 @@ async def upload_file(
     session.add(post)
     await session.commit()
     await session.refresh(post)
+    return post
 
+@app.get("/feed")
+async def get_feed(
+        session: AsyncSession = Depends(get_async_session)
+):
+    result = await session.execute(select(Post).order_by(Post.created_at.desc()))
+    posts = [row[0] for row in result.all()]
+
+    post_data = []
+    for post in posts:
+        post_data.append({
+            "id": str(post.id),
+            "caption": post.caption,
+            "url": post.url,
+            "file_type": post.file_type,
+            "file_name": post.file_name,
+            "created_at": post.created_at.isoformat()
+        })
+    return {"posts": post_data}
 # text_posts = {
 #     1: {
 #         "title": "Clean Code",
--- app/db.py
+++ app/db.py
@@ -31,5 +31,5 @@ async def create_db_and_tables():
 
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
-    async with async_session_maker as session:
+    async with async_session_maker() as session:
         yield session
